e61d3f17a7419fd0b94a85b6be6f2daafe0cd19e
Daniel Fischer <danieltgfischer@gmail.com>
2025-02-19 17:08:41 -0300

    feat: add smoth color transition

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,36 +1,92 @@
-import { useMemo } from "react";
-import { Text, TouchableWithoutFeedback } from "react-native";
+import { useEffect, useMemo, useState } from "react";
+import { Dimensions } from "react-native";
 import Animated, {
   FadeIn,
   useSharedValue,
   useAnimatedStyle,
   withTiming,
+  getRelativeCoords,
+  useAnimatedRef,
+  runOnJS,
+  FadeInUp,
 } from "react-native-reanimated";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 import { RandomColorGeneration } from "@/services";
 
-export default function Home() {
-  const backgroundColor = useSharedValue("#fff");
-  const randomColorGeneration = useMemo(() => new RandomColorGeneration(), []);
+const { height: screenHeight } = Dimensions.get("window");
 
-  const viewStyle = useAnimatedStyle(() => {
+export default function Home() {
+  const containerBackgroundColor = useSharedValue("#fff");
+  const animatedBackgroundColor = useSharedValue("#fff");
+  const circleSize = useSharedValue(0);
+  const animatedViewRef = useAnimatedRef();
+  const randomColorGeneration = useMemo(() => new RandomColorGeneration(), []);
+  const [coords, setCoords] = useState<{ x: number; y: number } | null>(null);
+
+  useEffect(() => {
+    if (coords) {
+      const randomColor = randomColorGeneration.getRandomColor();
+      circleSize.value = 0;
+      animatedBackgroundColor.value = randomColor ?? "#fff";
+      circleSize.value = withTiming(screenHeight, { duration: 3000 }, () => {
+        containerBackgroundColor.value = randomColor;
+      });
+    }
+  }, [coords]);
+
+  const tapGesture = Gesture.Tap().onEnd((event) => {
+    const relativeCoords = getRelativeCoords(
+      animatedViewRef,
+      event.absoluteX,
+      event.absoluteY
+    );
+
+    if (relativeCoords) {
+      runOnJS(setCoords)(relativeCoords);
+    }
+  });
+
+  const circleStyle = useAnimatedStyle(() => {
     return {
-      backgroundColor: withTiming(backgroundColor.value, { duration: 500 }),
+      width: circleSize.value,
+      height: circleSize.value,
+      borderRadius: circleSize.value / 2,
+      backgroundColor: animatedBackgroundColor.value,
+      transform: [{ scaleX: circleSize.value }, { scaleY: circleSize.value }],
+      position: "absolute",
+      top: coords?.y ?? 0,
+      left: coords?.x ?? 0,
     };
   });
 
-  const handlePress = () => {
-    backgroundColor.value = randomColorGeneration.getRandomColor();
-  };
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: containerBackgroundColor.value,
+    };
+  });
 
   return (
-    <TouchableWithoutFeedback onPress={handlePress}>
-      <Animated.View
-        entering={FadeIn.duration(1000)}
-        className="flex-1 items-center justify-center"
-        style={viewStyle}
-      >
-        <Text className="text-4xl font-bold">Hello there!</Text>
-      </Animated.View>
-    </TouchableWithoutFeedback>
+    <GestureHandlerRootView>
+      <GestureDetector gesture={tapGesture}>
+        <Animated.View
+          ref={animatedViewRef}
+          entering={FadeIn.duration(1000)}
+          className="flex-1 items-center justify-center"
+          style={containerStyle}
+        >
+          <Animated.Text
+            entering={FadeInUp.duration(1000)}
+            className="text-4xl font-bold z-10"
+          >
+            Hello there!
+          </Animated.Text>
+          <Animated.View style={[circleStyle]} />
+        </Animated.View>
+      </GestureDetector>
+    </GestureHandlerRootView>
   );
 }
