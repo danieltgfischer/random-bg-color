bf9fea64c38deaeb1f8db26f3aaf87e5156c8c77
Daniel Fischer <danieltgfischer@gmail.com>
2025-02-19 12:19:57 -0300

    chore: remove unused structure

---
+++ src/app/_layout.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import "react-native-reanimated";
+import { SafeAreaView } from "react-native";
+
+export { ErrorBoundary } from "expo-router";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    ...FontAwesome.font,
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <RootLayoutNav />
+    </SafeAreaView>
+  );
+}
+
+function RootLayoutNav() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="home" />
+    </Stack>
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function TabOneScreen() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
