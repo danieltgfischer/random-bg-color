4bd4ca40e953f96a4aa81381ee6f21e04e5b946c
Daniel Fischer <danieltgfischer@gmail.com>
2025-02-19 15:05:29 -0300

    feat: change bg color by taping

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,9 +1,36 @@
-import { View, Text } from "react-native";
+import { useMemo } from "react";
+import { Text, TouchableWithoutFeedback } from "react-native";
+import Animated, {
+  FadeIn,
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
+import { RandomColorGeneration } from "@/services";
 
 export default function Home() {
+  const backgroundColor = useSharedValue("#fff");
+  const randomColorGeneration = useMemo(() => new RandomColorGeneration(), []);
+
+  const viewStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: withTiming(backgroundColor.value, { duration: 500 }),
+    };
+  });
+
+  const handlePress = () => {
+    backgroundColor.value = randomColorGeneration.getRandomColor();
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <Animated.View
+        entering={FadeIn.duration(1000)}
+        className="flex-1 items-center justify-center"
+        style={viewStyle}
+      >
+        <Text className="text-4xl font-bold">Hello there!</Text>
+      </Animated.View>
+    </TouchableWithoutFeedback>
   );
 }
